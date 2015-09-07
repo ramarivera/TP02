d210284923a62585d4cc35b3e1525bb8133d1567
RamaRivera <ramarivera@gmail.com>
2015-09-07 04:43:45 -0300

    Segunda Version
    Testeo superficial realizado, correcciones menores a metodos Agregar,
    agregados algunos metodos de Strings.
    Reestructuracion del codigo con Regiones

--- EJ06/Fecha.cs
+++ EJ06/Fecha.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace EJ06
 {
+	/// <summary>
+	/// 
+	/// </summary>
 	class Fecha
 	{
+		#region Fecha - Atributos 
 		/// <summary>
 		/// 
 		/// </summary>
@@ -20,6 +24,8 @@ namespace EJ06
 		/// 
 		/// </summary>
 		private int iAnio;
+		#endregion
+		#region Fecha - Constantes
 		/// <summary>
 		/// 
 		/// </summary>
@@ -32,23 +38,37 @@ namespace EJ06
 		private static readonly int[] COEFICIENTES_MESES = { 6, 2, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
 		private static readonly int ANIO_MINIMO = 1900;
 		private static readonly int ANIO_MAXIMO = 2199;
-
+		#endregion
+		#region Fecha - Constructores
 		/// <summary>
-		/// 
+		/// Constructor de la clase Fecha
 		/// </summary>
-		/// <param name="pDia"></param>
-		/// <param name="pMes"></param>
-		/// <param name="pAnio"></param>
-		/// <exception cref="ArgumentException"> Arroja excepcion si el año esta fuera de rango</exception>
+		/// <param name="pDia">Numero de Dia</param>
+		/// <param name="pMes">Numero de Mes</param>
+		/// <param name="pAnio">Numero de Anio</param>
+		/// <exception cref="ArgumentOutOfRangeException"> Arroja excepcion si los valores de dias, mes o año no resultan validos</exception>
 		public Fecha(int pDia, int pMes, int pAnio)
 		{
 			if (pAnio < ANIO_MINIMO)
 			{
-				throw new System.ArgumentException("El Anio no puede ser menor a " + ANIO_MINIMO.ToString(), "pAnio");
-			}
+				throw new System.ArgumentOutOfRangeException("pAnio",pAnio,"El Anio no puede ser menor a " + ANIO_MINIMO.ToString());
+            }
 			if (pAnio > ANIO_MAXIMO)
 			{
-				throw new System.ArgumentException("El Anio no puede ser mayor a " + ANIO_MAXIMO.ToString(), "pAnio");
+				throw new System.ArgumentOutOfRangeException("pAnio", pAnio, "El Anio no puede ser mayor a " + ANIO_MAXIMO.ToString());
+			}
+			if (pMes < 1 || pMes > 12)
+			{
+				throw new System.ArgumentOutOfRangeException("pMes", pMes, "El numero de mes debe estar entre 01 y 12");
+			}
+			if (pDia > Fecha.DiasDelMesAnio(pMes, pAnio))
+			{
+				throw new System.ArgumentOutOfRangeException("pDia", pDia, "El numero de dias es mayor a los dias permitidos para el mes y anio");
+			}
+			if (pDia < 1)
+			{
+				throw new System.ArgumentOutOfRangeException("pMes", pMes, "El valor minimo de dia es 01");
+
 			}
 			this.iDia = pDia;
 			this.iMes = pMes;
@@ -57,24 +77,28 @@ namespace EJ06
 		}
 
 		public Fecha() : this(1, 1, ANIO_MINIMO) { }
-
+		#endregion
+		#region Fecha - Propiedades
+		/// <summary>
+		/// 
+		/// </summary>
 		public int Dia
 		{
 			get { return this.iDia; }
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
 		public int Mes
 		{
 			get { return this.iMes; }
 		}
+		/// <summary>
+		/// 
+		/// </summary>
 		public int Anio
 		{
 			get	{ return this.iAnio; }
-		}
-
-		public static bool EsBisiesto(int pAnio)
-		{
-			return (pAnio % 4 == 0 && pAnio % 100 != 0 || pAnio % 400 == 0);
 		}
 		/// <summary>
 		/// 
@@ -84,18 +108,100 @@ namespace EJ06
 		{
 			get { return Fecha.EsBisiesto(Anio); }
 		}
-		
-
-		public static int DiasDelMesAnio (int pMes, int pAnio)
-		{
-			return (pMes == 2 && Fecha.EsBisiesto(pAnio)) ? 29 : DIAS_MESES[pMes - 1]; 
-        }
-
+		/// <summary>
+		/// 
+		/// </summary>
 		public int DiasMesAnioActual
 		{
 			get { return DiasDelMesAnio(Mes, Anio); }
 		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public String DiaSemanaActual
+		{
+			get { return Fecha.DiaSemanaFecha(Dia, Mes, Anio); }
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public String NombreMesActual
+		{
+			get { return Fecha.NombreMes(Mes); }
+		}
+		#endregion
+		#region Fecha - Metodos Estaticos 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pAnio"></param>
+		/// <returns></returns>
+		public static bool EsBisiesto(int pAnio)
+		{
+			return (pAnio % 4 == 0 && pAnio % 100 != 0 || pAnio % 400 == 0);
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pMes"></param>
+		/// <param name="pAnio"></param>
+		/// <returns></returns>
+		public static int DiasDelMesAnio (int pMes, int pAnio)
+		{
+			return (pMes == 2 && Fecha.EsBisiesto(pAnio)) ? 29 : DIAS_MESES[pMes - 1]; 
+        }
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public static String DiaSemanaFecha(int pDia, int pMes, int pAnio)
+		{
+			int resultado = 0;
 
+			//
+			if (pAnio >= 1900 && pAnio < 2000)
+			{
+				resultado++;
+			}
+			else
+			{
+				if (pAnio >= 2100 && pAnio < 2200)
+				{
+					resultado -= 2;
+				}
+			}
+
+			//
+			resultado += ((pAnio % 100) + (pAnio % 100) / 4);
+
+			//
+			resultado += Fecha.EsBisiesto(pAnio) ? -1 : 0;
+
+			//
+			resultado += COEFICIENTES_MESES[pMes - 1];
+
+			//
+			resultado += pDia;
+
+			//
+			return NOMBRES_DIAS[resultado % 7];
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pMes"></param>
+		/// <returns></returns>
+		public static String NombreMes(int pMes)
+		{
+			return NOMBRES_MESES[pMes - 1];
+		}
+		#endregion
+		#region Fecha - Metodos "Agregar"
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pDias"></param>
+		/// <returns></returns>
 		public Fecha AgregarDias(int pDias)
 		{
 			int maxDias, nuevoDia;
@@ -111,23 +217,28 @@ namespace EJ06
 			}
 			else
 			{
-				Fecha auxFecha = this.AgregarMeses(1);
-				return auxFecha.AgregarDias(nuevoDia - maxDias);
+				Fecha auxFecha = new Fecha(1, Mes, Anio);
+				auxFecha = auxFecha.AgregarMeses(1);
+				return auxFecha.AgregarDias(nuevoDia - maxDias - 1);
 			}
 
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pMeses"></param>
+		/// <returns></returns>
 		public Fecha AgregarMeses(int pMeses)
 		{
 			if (pMeses > 1)
 			{
 				// int auxMes = pMeses - 1;
 				Fecha auxFecha = this.AgregarMeses(1);
-				return auxFecha.AgregarMeses(pMeses--);
+				return auxFecha.AgregarMeses(pMeses-1);
 			}
 			else
 			{
-				if (DiasMesAnioActual == 31)
+				if (Dia == 31)
 				{
 					if (Mes == 12)
 					{
@@ -146,7 +257,11 @@ namespace EJ06
 				}
 			}
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pAnio"></param>
+		/// <returns></returns>
 		public Fecha AgregarAnio (int pAnio)
 		{
 			int nuevoDia, nuevoMes, nuevoAnio;
@@ -175,49 +290,13 @@ namespace EJ06
 			return new Fecha(nuevoDia, nuevoMes, nuevoAnio);
 			
 		}
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <returns></returns>
-		public String DiaSemanaFecha()
+		#endregion
+		#region Fecha - Metodos Sobrecargados (Equals, ToString, GetHashCode)
+		public override string ToString()
 		{
-			int resultado = 0;
-			int anio = Anio;
-
-			//
-			if (anio >= 1900 && anio < 2000)
-			{
-				resultado++;
-			}
-			else
-			{
-				if (anio >= 2100 && anio < 2200)
-				{
-					resultado -= 2;
-				}
-			}
-
-			//
-			resultado += ((anio % 100) + (anio % 100) / 4);
-
-			//
-			resultado += this.EsBisiesto() ? -1 : 0;
-
-			//
-			resultado += COEFICIENTES_MESES[Mes - 1];
-
-			//
-			resultado += Dia;
-
-			//
-			return NOMBRES_DIAS[resultado % 7];
-        }
-
-		private bool EsIgual (Fecha pOtraFecha)
-		{
-			return (Anio == pOtraFecha.Anio && Mes == pOtraFecha.Mes && Dia == pOtraFecha.Dia);
+			return Dia.ToString() + "/" + Mes.ToString() + "/" + Anio.ToString();
 		}
-
+		
 		/// <summary>
 		/// Sobrecarga del metodo Equals()
 		/// </summary>
@@ -225,62 +304,90 @@ namespace EJ06
 		/// <returns>Verdadero o Falso, segun la igualdad del objeto</returns>
 		public override bool Equals(object obj)
 		{
+			// Si obj es (apunta a) null, falso
 			if (Object.ReferenceEquals(null, obj))
 			{
 				return false;
 			}
-
+			// Si obj es (apunta a) this, verdadero
 			if (Object.ReferenceEquals(this, obj))
 			{
 				return true;
 			}
 
+			// Si los tipos son distintos, falso
+			// Esto me permite castear con () sin controlar por (obj)null
 			if (obj.GetType() != this.GetType())
 			{
 				return false;
 			}
 
-			// Aplico logica particular
+			// Aplico logica particular, casteando previamente a Fecha
 			return (this.EsIgual((Fecha)obj));
 		}
-
-		public bool Equals(Fecha fecha)
+		/// <summary>
+		/// Metodo Equals para objetos de la clase Fecha
+		/// </summary>
+		/// <param name="pFecha">Fecha con la que desea controlar igualdad (NO IDENTIDAD)</param>
+		/// <returns>Verdadero o Falso, dependiendo la igualdad de los elementos</returns>
+		public bool Equals(Fecha pFecha)
 		{
-			// Is null?
-			if (Object.ReferenceEquals(null, fecha))
+			// Si pFecha es (apunta a) null, falso
+			if (Object.ReferenceEquals(null, pFecha))
 			{
 				return false;
 			}
 
-			// Is the same object?
-			if (Object.ReferenceEquals(this, fecha))
+			// Si pFecha es (apunta a) this, verdadero
+			if (Object.ReferenceEquals(this, pFecha))
 			{
 				return true;
 			}
 
-			return (this.EsIgual(fecha));
+			// Aplico logica particular
+			return (this.EsIgual(pFecha));
 		}
-
 		/// <summary>
 		/// Sobrecarga del metodo GetHashCode()
 		/// http://www.loganfranken.com/blog/692/overriding-equals-in-c-part-2/
+		/// El HashCode debe ser rapido de calcular y con pocas colisiones
 		/// </summary>
 		/// <returns>Integer HashCode</returns>
 		public override int GetHashCode()
 		{
+			// Buscamos grandes productos semi-aleatorios, por lo tanto somos concientes de que un overflow de integers es posible, el cual no nos afecta
 			unchecked
 			{
-				const int HashingBase = (int)2166136261;
-				const int HashingMultiplier = 16777619;
+				// Un gran numero primo disminuye las colisiones en grandes conjuntos de objetos
+				const int HashingBase = (int)2166136261; //Primo01, casteado a int
+				const int HashingMultiplier = 16777619; //Primo02
 
 				int hash = HashingBase;
+				//Utilizamos cada propiedad de nuestro objeto, si dicha propiedad es nula, el resultado es 0
 				hash = (hash * HashingMultiplier) ^ (!Object.ReferenceEquals(null, Dia) ? Dia.GetHashCode() : 0);
+				//Sucesivamente vamos acumulando los resultados
 				hash = (hash * HashingMultiplier) ^ (!Object.ReferenceEquals(null, Mes) ? Mes.GetHashCode() : 0);
+				//Por ultimo en vez de usar +, usamos el operador XOR ^ para obtener una implementacion mas performante
 				hash = (hash * HashingMultiplier) ^ (!Object.ReferenceEquals(null, Anio) ? Anio.GetHashCode() : 0);
 				return hash;
 			}
 		}
-
+		#endregion
+		#region Fecha - Metodos de Comparacion
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pOtraFecha"></param>
+		/// <returns></returns>
+		private bool EsIgual(Fecha pOtraFecha)
+		{
+			return (Anio == pOtraFecha.Anio && Mes == pOtraFecha.Mes && Dia == pOtraFecha.Dia);
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pOtraFecha"></param>
+		/// <returns></returns>
 		private bool EsMayor(Fecha pOtraFecha)
 		{
 			// 12/11/2012 y 04/10/2012
@@ -310,53 +417,95 @@ namespace EJ06
 			}
 			return true;
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pOtraFecha"></param>
+		/// <returns></returns>
 		private bool EsMenor(Fecha pOtraFecha)
 		{
 			return !(this.EsMayor(pOtraFecha) || this.EsIgual(pOtraFecha));
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pOtraFecha"></param>
+		/// <returns></returns>
 		private bool EsMayorIgual(Fecha pOtraFecha)
 		{
 			return (this.EsIgual(pOtraFecha) || this.EsMayor(pOtraFecha));
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pOtraFecha"></param>
+		/// <returns></returns>
 		private bool EsMenorIgual(Fecha pOtraFecha)
 		{
 			return !this.EsMayor(pOtraFecha);
 		}
-
+		#endregion
+		#region Fecha - Operadores
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pA"></param>
+		/// <param name="pB"></param>
+		/// <returns></returns>
 		public static bool operator ==(Fecha pA, Fecha pB)
 		{
 			return pA.EsIgual(pB);
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pA"></param>
+		/// <param name="pB"></param>
+		/// <returns></returns>
 		public static bool operator !=(Fecha pA, Fecha pB)
 		{
 			return !(pA == pB);
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pA"></param>
+		/// <param name="pB"></param>
+		/// <returns></returns>
 		public static bool operator >(Fecha pA, Fecha pB)
 		{
 			return pA.EsMayor(pB);
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pA"></param>
+		/// <param name="pB"></param>
+		/// <returns></returns>
 		public static bool operator <(Fecha pA, Fecha pB)
 		{
 			return pA.EsMenor(pB);
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pA"></param>
+		/// <param name="pB"></param>
+		/// <returns></returns>
 		public static bool operator >=(Fecha pA, Fecha pB)
 		{
 			return pA.EsMayorIgual(pB);
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pA"></param>
+		/// <param name="pB"></param>
+		/// <returns></returns>
 		public static bool operator <=(Fecha pA, Fecha pB)
 		{
 			return pA.EsMenorIgual(pB);
 		}
-
-
-
+		#endregion
 	}
 }
--- EJ06/Program.cs
+++ EJ06/Program.cs
@@ -8,8 +8,119 @@ namespace EJ06
 {
 	class Program
 	{
+		static void GoodBye()
+		{
+			Console.Clear();
+			Console.WriteLine("\n\n\n\n\n\n\n\n\n\n                    Thank you for trusting MARR Systems Inc.  ");
+			Console.ReadKey();
+		}
+		/// <summary>
+		/// Muestra una cadena de caracteres, utilizado como separador en los menues principales por consola
+		/// </summary>
+		static void SeparadorMenuPrincipal()
+		{
+			Console.WriteLine("\n************************** Menu Principal *************************\n");
+		}
+		/// <summary>
+		/// Muestra una cadena de caracteres, utilizado como separador en los menues secundarios por consola
+		/// </summary>
+		static void SeparadorOperatoria()
+		{
+			Console.WriteLine("\n-------------------Operando------------------\n");
+		}
+
+		static Fecha CargarFecha()
+		{
+			int dia, mes, anio;
+			Console.Write("\t Dia: ");
+			dia = int.Parse(Console.ReadLine());
+			Console.Write("\t Mes: ");
+			mes = int.Parse(Console.ReadLine());
+			Console.Write("\t Año: ");
+			anio = int.Parse(Console.ReadLine());
+			return new Fecha(dia, mes, anio);
+		}
 		static void Main(string[] args)
 		{
+			Fecha lFecha1, lFecha2;
+            SeparadorMenuPrincipal();
+			Console.WriteLine("Ingrese la fecha inicial");
+			lFecha1 = CargarFecha();
+            bool seguir = true;
+			while (seguir)
+			{
+				int aux = 0;
+				SeparadorOperatoria();
+				Console.WriteLine("Ingrese la Operacion que desea realizar");
+				Console.WriteLine("1:\t Mostrar Fecha");
+				Console.WriteLine("2:\t Agregar Dias");
+				Console.WriteLine("3:\t Agregar Meses");
+				Console.WriteLine("4:\t Agregar Años");
+				Console.WriteLine("5:\t Comparar con");
+				Console.WriteLine("0:\t Salir");
+				Console.Write("Opcion elegida: ");
+				switch (int.Parse(Console.ReadLine()))
+				{
+					case 1:
+						Console.WriteLine("La cadena que representa a la fecha es: {0}",lFecha1.ToString());
+						Console.WriteLine("El dia es {0} y el mes es {1}",lFecha1.DiaSemanaActual,lFecha1.NombreMesActual);
+						Console.ReadKey();
+						Console.WriteLine();
+						break;
+					case 2:
+						Console.Write("Ingrese la cantidad de dias que desea agregar: ");
+						aux = int.Parse(Console.ReadLine());
+						lFecha1 = lFecha1.AgregarDias(aux);
+						Console.WriteLine();
+						break;
+					case 3:
+						Console.Write("Ingrese la cantidad de meses que desea agregar: ");
+						aux = int.Parse(Console.ReadLine());
+						lFecha1 = lFecha1.AgregarMeses(aux);
+						Console.WriteLine();
+						break;
+					case 4:
+						Console.Write("Ingrese la cantidad de anios que desea agregar: ");
+						aux = int.Parse(Console.ReadLine());
+						lFecha1 = lFecha1.AgregarAnio(aux);
+						Console.WriteLine();
+						break;
+					case 5:
+						lFecha2 = CargarFecha();
+						if (lFecha2 == lFecha1)
+						{
+							Console.WriteLine("Las fechas son Iguales");
+						}
+						else
+						{
+							Console.WriteLine("Las fechas son distintas");
+						}
+						if (lFecha1>lFecha2)
+						{
+							Console.WriteLine("Fecha01 es mayor que Fecha02");
+						}
+						else
+						{
+							if (lFecha1!=lFecha2 && lFecha1<lFecha2)
+							{
+								Console.WriteLine("Fecha 02 es mayor que Fecha01");
+							}
+						}
+						Console.ReadKey();
+						Console.WriteLine();
+						break;
+					case 0:
+						seguir = false;
+						break;
+					default:
+						Console.Write("Opcion incorrecta. Reintente\n");
+						Console.ReadKey();
+						Console.WriteLine();
+						break;
+				}
+			}
+			GoodBye();
 		}
 	}
 }
+
